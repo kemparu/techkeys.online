d2af14a018035ea3cb416dae35821f4900169303
Chandu J S <app.coder@outlook.in>
2020-06-24 18:26:45 +0530

    tsx pages & editor configs

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,10 +1,17 @@
 module.exports = {
   siteMetadata: {
-    title: `Gatsby Default Starter`,
-    description: `Kick off your next, great Gatsby project with this default starter. This barebones starter ships with the main Gatsby configuration files you might need.`,
-    author: `@gatsbyjs`,
+    title: `TechKeys`,
+    description: `TechKeys Online`,
+    author: `@techkeysonline`,
   },
   plugins: [
+    {
+      resolve: `gatsby-plugin-typescript`,
+      options: {
+        isTSX: true,
+        allExtensions: true,
+      },
+    },
     `gatsby-plugin-react-helmet`,
     {
       resolve: `gatsby-source-filesystem`,
@@ -15,20 +22,5 @@ module.exports = {
     },
     `gatsby-transformer-sharp`,
     `gatsby-plugin-sharp`,
-    {
-      resolve: `gatsby-plugin-manifest`,
-      options: {
-        name: `techkeys`,
-        short_name: `starter`,
-        start_url: `/`,
-        background_color: `#663399`,
-        theme_color: `#663399`,
-        display: `minimal-ui`,
-        icon: `src/images/gatsby-icon.png`, // This path is relative to the root of the site.
-      },
-    },
-    // this (optional) plugin enables Progressive Web App + Offline functionality
-    // To learn more, visit: https://gatsby.dev/offline
-    // `gatsby-plugin-offline`,
   ],
 }
---
+++ src/pages/index.tsx
@@ -0,0 +1,9 @@
+import React, { Component } from 'react'
+
+import MainLayout from '../layouts/main'
+
+export default class IndexPage extends Component {
+  render() {
+    return <MainLayout>Welcome</MainLayout>
+  }
+}
